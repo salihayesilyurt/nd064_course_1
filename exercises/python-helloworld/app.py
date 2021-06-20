c13fe4e405629d83116479a837a3ee9f8364c44f
salihayesilyurt <yesilyurt.saliha@gmail.com>
2021-06-20 15:45:27 +0300

    Exercise: Application Logging
    app.log file, DEBUG level.

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask import json
+import logging
+
 
 app = Flask(__name__)
 
 @app.route('/status')
-def status():
+def healthcheck():
     response = app.response_class(
             response=json.dumps({"result":"OK - healthy"}),
             status=200,
             mimetype='application/json'
     )
+
+    #Log line
+    app.logger.info("Status endpoint was reached")
 
     return response
 
@@ -21,11 +26,15 @@ def metrics():
             mimetype='application/json'
     )
 
+    app.logger.info("Metrics endpoint was reached") 
     return response
 
 @app.route("/")
 def hello():
+    app.logger.info("Main endpoint was reached") 
     return "Hello World!"
 
 if __name__ == "__main__":
+
+    logging.basicConfig(filename='app.log',format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
     app.run(host='0.0.0.0')
